1b161fe673438faffb3848c1eee80847fde7acfe
liuj-s <liuj-s@glodon.com>
2017-10-30 10:45:48 +0800

    sth

--- src/main/java/com/docker/test/SampleController.java
+++ src/main/java/com/docker/test/SampleController.java
@@ -5,11 +5,17 @@ package com.docker.test;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@Configuration
 @EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableConfigServer
 public class SampleController {
 
     @RequestMapping("/")
